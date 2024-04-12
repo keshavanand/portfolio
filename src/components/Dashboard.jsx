a160ea296e3610b999956d5b42e0f53290cc6b9a
keshavanand <keshavanandsingh89@gmail.com>
2024-04-11 22:00:01 -0400

    added dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,7 +1,26 @@
+import { LuRocket } from "react-icons/lu";
 export function Dashboard(){
     return (
-        <div>
-            Hey there
+        <div id="dashboard" className="pt-20 mx-64 animate-moving-line ">
+            <div className=" text-indigo-600 text-2xl sm:text-3xl font-bold">
+                Hey there! My name is
+            </div>
+            <div className=" text-black text-6xl sm:text-8xl lg:text-9xl font-bold">
+                Keshav Anand Singh. 
+            </div>
+            <div className=" text-black text-2xl sm:text-3xl font-medium sm:font-bold pt-10">
+                Software Engineer.
+                <span className="text-gray-700 dark:text-indigo-500 text-2xl text-center sm:text-3xl font-medium sm:font-bold pt-10"> A Full-Stack Developer with an interest in Artifical Intelligence</span>
+            </div>
+            <div className=" text-black flex flex-row text-md md:text-lg lg:text-2xl font-md md:font-lg lg:font-xl pt-10">
+                <div className="text-4xl my-auto text-indigo-500"><LuRocket></LuRocket></div>
+                <p className="ml-5 mt-1 text-center font-semibold text-xl sm:text-2xl">Currently exploring new opportunities and working on some side projects.</p>
+            </div>
+            <div>
+                <button>Github</button>
+                <button>Linkedin</button>
+                <button>E-mail</button>
+            </div>
         </div>
     )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export function Header(){
     return (
         <div className="animate-moving-line px-56 mt-4  h-12 grid grid-cols-12 gap-36 shadow-md ">
             <div className="text-xl text-center text-gray-600 w-40 mb-2 pt-1 duration-700 hover:text-black hover:bg-gray-200 hover:text-2xl active:text-base active:duration-100 rounded-lg">
-                <a href="#">Dashboard</a>
+                <a href="#dashboard">Dashboard</a>
             </div>
             <div className="text-xl text-center text-gray-600 w-40 mb-2 pt-1 duration-700 hover:text-black hover:bg-gray-200 hover:text-2xl active:text-base active:duration-100 rounded-lg">
                 <a href="#">Projects</a>
