31bb5437703072cca1ddffeb847c49cbb5041fd7
keshavanand <keshavanandsingh89@gmail.com>
2025-08-11 10:19:55 -0400

    added analytics vercel 2

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import { About } from "./components/About"
 import { Projects } from "./components/Projects"
 import { Contact } from "./components/Contact"
 import { Footer } from "./components/Footer"
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <div className="min-h-screen bg-slate-50 dark:bg-slate-900 transition-colors duration-300">
+      <Analytics/>
       <Header />
       <main>
         <Hero />
